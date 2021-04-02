b9de31de2e9443b049763adf015046647339a9c4
Vishnukumar Ksd <ksdvishnukumar1987@gmail.com>
2021-04-02 22:48:22 +0530

    Add project files.

---
+++ CustomerFunc.Azure.Storage.Table.Read.Client/CustomerProfile.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace CustomerFunc.Azure.Storage.Table.Read.Client
+{
+    public class CustomerProfile : TableEntity
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomerProfile"/> class.
+        /// </summary>
+        public CustomerProfile()
+        {
+
+        }
+        /// <summary>
+        /// The customer identifier
+        /// </summary>
+        public int customerID { get; set; }
+
+        /// <summary>
+        /// The first name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The middle name
+        /// </summary>
+        public string MiddleName { get; set; }
+
+        /// <summary>
+        /// The last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The mobile number
+        /// </summary>
+        public string MobileNumber { get; set; }
+
+        /// <summary>
+        /// The customer type
+        /// </summary>
+        public string CustomerType { get; set; }
+
+        /// <summary>
+        /// Assigns the row key.
+        /// </summary>
+        private void AssignRowKey()
+        {
+            this.RowKey = customerID.ToString();
+        }
+
+        /// <summary>
+        /// Assigns the partition key.
+        /// </summary>
+        private void AssignPartitionKey()
+        {
+            this.PartitionKey = CustomerType;
+        }
+    }
+}
---
+++ CustomerFunc.Azure.Storage.Table.Read.Client/Program.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CustomerFunc.Azure.Storage.Table.Read.Client
+{
+    public class Program
+    {
+        /// <summary>
+        /// The table
+        /// </summary>
+        private static CloudTable table;
+
+        private static IConfigurationRoot config;
+        static async Task Main(string[] args)
+        {
+            config = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory).AddJsonFile("appsettings.json", optional: true, reloadOnChange: false).AddEnvironmentVariables().Build();
+            ConnectToCloudTable();
+            string partitionKey = "Client1";
+            string rowkey = "1";
+            await RetrieveRecord(table, partitionKey, rowkey);
+
+            var customerProfiles = await GetAllEntitiesFromTable<CustomerProfile>(table);
+            foreach (var customer in customerProfiles)
+            {
+                Console.WriteLine($"{nameof(customer.customerID)} {customer.RowKey}");
+                Console.WriteLine($"{nameof(customer.CustomerType)} {customer.PartitionKey}");
+                Console.WriteLine($"{nameof(customer.FirstName)} {customer.FirstName}");
+                Console.WriteLine($"{nameof(customer.MiddleName)} {customer.MiddleName}");
+                Console.WriteLine($"{nameof(customer.LastName)} {customer.LastName}");
+                Console.WriteLine($"{nameof(customer.MobileNumber)} {customer.MobileNumber}");
+                Console.WriteLine("----------------------------------------------------------------");
+            }
+        }
+
+        private static void ConnectToCloudTable()
+        {
+            var connectionString = config["StorageAccountConnectionString"];
+            var storageAccount = CloudStorageAccount.Parse(connectionString);
+            var tableClient = storageAccount.CreateCloudTableClient();
+            table = tableClient.GetTableReference(config["TableName"]);
+        }
+
+        public async static Task RetrieveRecord(CloudTable table, string partitionKey, string rowKey)
+        {
+            TableOperation tableOperation = TableOperation.Retrieve<CustomerProfile>(partitionKey, rowKey);
+            TableResult tableResult = await table.ExecuteAsync(tableOperation);
+            var profile = tableResult.Result as CustomerProfile;
+
+            if (profile != null)
+            {
+                Console.WriteLine($"{nameof(profile.customerID)} {profile.RowKey}");
+                Console.WriteLine($"{nameof(profile.CustomerType)} {profile.PartitionKey}");
+                Console.WriteLine($"{nameof(profile.FirstName)} {profile.FirstName}");
+                Console.WriteLine($"{nameof(profile.MiddleName)} {profile.MiddleName}");
+                Console.WriteLine($"{nameof(profile.LastName)} {profile.LastName}");
+                Console.WriteLine($"{nameof(profile.MobileNumber)} {profile.MobileNumber}");
+                Console.WriteLine("----------------------------------------------------------------");
+            }
+        }
+
+        private static async Task<IEnumerable<T>> GetAllEntitiesFromTable<T>(CloudTable table) where T : ITableEntity, new()
+        {
+            TableQuerySegment<T> querySegment = null;
+            var entities = new List<T>();
+            var query = new TableQuery<T>();
+
+            do
+            {
+                querySegment = await table.ExecuteQuerySegmentedAsync(query, querySegment?.ContinuationToken);
+                entities.AddRange(querySegment.Results);
+            } while (querySegment.ContinuationToken != null);
+
+            return entities;
+        }
+    }
+}
